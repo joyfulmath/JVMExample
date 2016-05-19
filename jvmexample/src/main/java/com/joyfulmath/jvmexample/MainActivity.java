729f6567507ed60bf703f28c507d6f5d616f3a42
luyuanchun032 <luyuanchun032@pingan.com.cn>
2016-05-19 13:49:56 +0800

    rename app to jvmexample

--- jvmexample/src/main/java/com/joyfulmath/jvmexample/MainActivity.java
+++ jvmexample/src/main/java/com/joyfulmath/jvmexample/MainActivity.java
@@ -3,11 +3,14 @@ package com.joyfulmath.jvmexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.joyfulmath.jvmexample.excutor.StaticResolution;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StaticResolution.sayHello();
     }
 }
---
+++ jvmexample/src/main/java/com/joyfulmath/jvmexample/excutor/StaticResolution.java
@@ -0,0 +1,17 @@
+package com.joyfulmath.jvmexample.excutor;
+
+import com.joyfulmath.jvmexample.TraceLog;
+
+/**
+ * static method example
+ * @author deman.lu
+ * @version on 2016-05-19 11:48
+ */
+public class StaticResolution {
+
+    public static void sayHello()
+    {
+        TraceLog.i();
+    }
+
+}
